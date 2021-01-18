5f0b31a4e5842108b22a0e238f14f9127cc8e66a
Mohanlal Gopu <mohanlal.gopu@syncfusion.com>
2021-01-18 16:40:04 +0530

    Update new version.

---
+++ Chart_Sqlite_Sample/Chart_Sqlite_Sample/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chart_Sqlite_Sample
+{
+    public partial class App : Application
+    {
+        static ChartDatabase database;
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+        public static ChartDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ChartDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ChartDataBase.db3"));
+                }
+                return database;
+            }
+        }
+
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Chart_Sqlite_Sample/Chart_Sqlite_Sample/ChartDataModel.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chart_Sqlite_Sample
+{
+    public class ChartDataModel
+    {
+        [PrimaryKey]
+        public string XValue { get; set; }
+        public double YValue { get; set; }
+    }
+}
